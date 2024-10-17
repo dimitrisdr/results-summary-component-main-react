72fd105b10044393e9802025f7e6782473419374
Dimitris_Dr <jidrist18@hotmail.com>
2024-10-17 12:43:37 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/results-summary-component-main-react/',
+  plugins: [react()]
 })
